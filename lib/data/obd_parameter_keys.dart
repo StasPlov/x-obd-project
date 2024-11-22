7da2941b91b59245049e18e897c80c36ca1029bd
StasPlov <SaviouR.S@mail.ru>
2024-11-23 01:45:03 +1000

    update: sensor list

--- lib/data/obd_parameter_keys.dart
+++ lib/data/obd_parameter_keys.dart
@@ -5,16 +5,54 @@ class ObdParameterKeys {
 	static const String coolantTemp = 'coolantTemp';
 	static const String timing = 'timing';
 	static const String intakeTemp = 'intakeTemp';
+	static const String engineOilTemp = 'engineOilTemp';
+	static const String manifoldPressure = 'manifoldPressure';
+	static const String engineRuntime = 'engineRuntime';
+	static const String engineTorque = 'engineTorque';
+	static const String oilPressure = 'oilPressure';
+	static const String engineOilLife = 'engineOilLife';
+	static const String turboBoost = 'turboBoost';
 	
 	// Движение
 	static const String speed = 'speed';
 	static const String throttle = 'throttle';
+	static const String absThrottle = 'absThrottle';
+	static const String wheelSpeed = 'wheelSpeed';
+	static const String accelerationX = 'accelerationX';
+	static const String accelerationY = 'accelerationY';
+	static const String steeringAngle = 'steeringAngle';
 	
 	// Топливная система
 	static const String fuelLevel = 'fuelLevel';
 	static const String maf = 'maf';
 	static const String o2Voltage = 'o2Voltage';
+	static const String fuelPressure = 'fuelPressure';
+	static const String fuelRailPressure = 'fuelRailPressure';
+	static const String fuelConsumption = 'fuelConsumption';
+	static const String fuelTrim = 'fuelTrim';
+	static const String fuelType = 'fuelType';
+	static const String ethanolPercent = 'ethanolPercent';
+	static const String fuelRate = 'fuelRate';
 	
 	// Электрика
 	static const String voltage = 'voltage';
+	static const String batteryVoltage = 'batteryVoltage';
+	static const String alternatorVoltage = 'alternatorVoltage';
+	static const String batteryTemp = 'batteryTemp';
+	static const String batteryCharge = 'batteryCharge';
+	static const String alternatorLoad = 'alternatorLoad';
+	
+	// Трансмиссия
+	static const String transmissionTemp = 'transmissionTemp';
+	static const String currentGear = 'currentGear';
+	static const String torqueConverter = 'torqueConverter';
+	static const String transmissionSlip = 'transmissionSlip';
+	static const String clutchPosition = 'clutchPosition';
+	
+	// Выхлопная система
+	static const String catalystTemp = 'catalystTemp';
+	static const String exhaustGasTemp = 'exhaustGasTemp';
+	static const String noxSensor = 'noxSensor';
+	static const String particulateFilter = 'particulateFilter';
+	static const String egr = 'egr';
 } 
